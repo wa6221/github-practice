a17cd9b70e31e527b150398bada6486ba71091ac
rlagnlfo <hramst0618@gmail.com>
2024-06-27 16:18:49 +0900

    index.java의 세번째 코드 작성

--- index.java
+++ index.java
@@ -2,5 +2,6 @@ public class index {
     public static void main(String[] args) {
         System.out.println("HELLO");
         System.out.println("HELLO");
+        System.out.println("HELLO");
     }
 }
